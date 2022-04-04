c2f2d7d1bfbb6885071efc420fa7ce2b6201ef25
LordBael <juan.inga.est@tecazuay.edu.ec>
2022-04-03 22:08:34 -0500

    CRUD completado

--- app/src/main/java/com/example/tarea_app_sqlite/MainActivity.java
+++ app/src/main/java/com/example/tarea_app_sqlite/MainActivity.java
@@ -1,68 +1,95 @@
 package com.example.tarea_app_sqlite;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
 import android.os.Bundle;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.tarea_app_sqlite.modelo.DbHelper;
-import com.example.tarea_app_sqlite.modelo.Persona;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCrear;
-    Button btnGrabar;
-    EditText txtID, txtNombre, txtCorreo;
-
+    EditText name, contact, dob, lastname;
+    Button insert, update, delete, view;
+    DbHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnCrear=findViewById(R.id.button1);
-        //grabar
-        btnGrabar= findViewById(R.id.buttonGrabar);
-        txtID= findViewById(R.id.editTextTextPersonID);
-        txtNombre= findViewById(R.id.editTextTextPersonNombre);
-        txtCorreo= findViewById(R.id.editTextTextPersonCorreo);
-        btnCrear.setOnClickListener(new View.OnClickListener() {
+        name = findViewById(R.id.nombre);
+        lastname=findViewById(R.id.apellido);
+        contact = findViewById(R.id.telefono);
+        dob = findViewById(R.id.fecha_nacimiento);
+        insert = findViewById(R.id.btnInsert);
+        update = findViewById(R.id.btnUpdate);
+        delete = findViewById(R.id.btnDelete);
+        view = findViewById(R.id.btnView);
+        DB = new DbHelper(this);
+        insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String lastnameTXT = lastname.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
 
+                Boolean checkinsertdata = DB.insertuserdata(nameTXT,lastnameTXT, contactTXT, dobTXT);
+                if(checkinsertdata==true)
+                    Toast.makeText(MainActivity.this, "Nuevo usuario ingresado", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Usuario no ingresado", Toast.LENGTH_SHORT).show();
+            }        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String lastnameTXT = lastname.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
 
-                //crear bd
+                Boolean checkupdatedata = DB.updateuserdata(nameTXT,lastnameTXT, contactTXT, dobTXT);
+                if(checkupdatedata==true)
+                    Toast.makeText(MainActivity.this, "Usuario actualizado", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Usuario no actualizado", Toast.LENGTH_SHORT).show();
+            }        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                Boolean checkudeletedata = DB.deletedata(nameTXT);
+                if(checkudeletedata==true)
+                    Toast.makeText(MainActivity.this, "Usuario eliminado", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Usuario no eliminado", Toast.LENGTH_SHORT).show();
+            }        });
 
-                DbHelper dbHelper = new DbHelper(getApplicationContext());
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                if (db != null) {
-                    Toast.makeText(getApplicationContext(), "BASE CREADA", Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(getApplicationContext(), "ERROR AL CREAR LA BASE", Toast.LENGTH_LONG).show();
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+                if(res.getCount()==0){
+                    Toast.makeText(MainActivity.this, "No existen usuarios", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-            }
-        });
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Nombre :"+res.getString(0)+"\n");
+                    buffer.append("Apellido :"+res.getString(1)+"\n");
+                    buffer.append("Telefono :"+res.getString(2)+"\n");
+                    buffer.append("Fecha de nacimiento :"+res.getString(3)+"\n\n");
+                }
 
-        //grabar
-        btnGrabar= findViewById(R.id.buttonGrabar);
-        txtID= findViewById(R.id.editTextTextPersonID);
-        txtNombre= findViewById(R.id.editTextTextPersonNombre);
-        txtCorreo= findViewById(R.id.editTextTextPersonCorreo);
-        btnGrabar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //grabar
-                Persona p =new Persona();
-                p.setId( Integer.parseInt( txtID.getText().toString()));
-                p.setNombre(txtNombre.getText().toString());
-                p.setCorreo(txtCorreo.getText().toString());
-                p.guardar(getApplicationContext());
-                Toast.makeText(getApplicationContext(), "Persona Creada", Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-    }
-}
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("Informacion de usuarios");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }        });
+    }}
--- app/src/main/java/com/example/tarea_app_sqlite/modelo/DbHelper.java
+++ app/src/main/java/com/example/tarea_app_sqlite/modelo/DbHelper.java
@@ -1,53 +1,79 @@
 package com.example.tarea_app_sqlite.modelo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Build;
 import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
-    private static  final String DATABASE_NOMBRE="bsesql.db";
-    private static final int DATABASE_VERSION=2;
-    private static final String TABLA_PERSONAS="personas";
-    public DbHelper(@Nullable Context context) {
-        super(context, DATABASE_NOMBRE,null,DATABASE_VERSION );
+    public DbHelper(Context context) {
+        super(context, "Userdata.db", null, 1);
     }
-
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql= "CREATE TABLE "+ TABLA_PERSONAS + " (" +
-                "id INTEGER NOT NULL," +
-                " nombre TEXT NOT NULL," +
-                "telefono TEXT NOT NULL," +
-                " correo TEXT," +
-                "edad INTEGER " +
-                ")";
-
-        sqLiteDatabase.execSQL(sql);
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table Personas(name TEXT primary key, lastname TEXT, contact TEXT, dob TEXT)");
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
-        String sqld ="DROP TABLE " + TABLA_PERSONAS;
-        sqLiteDatabase.execSQL(sqld);
-        onCreate(sqLiteDatabase);
-
+    public void onUpgrade(SQLiteDatabase DB, int i, int ii) {
+        DB.execSQL("drop Table if exists Personas");
+    }
+    public Boolean insertuserdata(String name,String lastname, String contact, String dob)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("lastname",lastname);
+        contentValues.put("contact", contact);
+        contentValues.put("dob", dob);
+        long result=DB.insert("Personas", null, contentValues);
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    public Boolean updateuserdata(String name,String lastname, String contact, String dob)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("lastname",lastname);
+        contentValues.put("contact", contact);
+        contentValues.put("dob", dob);
+        Cursor cursor = DB.rawQuery("Select * from Personas where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.update("Personas", contentValues, "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+    public Boolean deletedata (String name)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Personas where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.delete("Personas", "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 
-    public void no_query (String nsql) {
-        this.getWritableDatabase().execSQL(nsql);
-
-
-    }
-
-    public Cursor query(String sql){
-
-
-        return this.getReadableDatabase().rawQuery(sql,null);
-
+    public Cursor getdata ()
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Personas", null);
+        return cursor;
     }
 
 }
